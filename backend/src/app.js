3cd207297520b480987a5e3224bd3ab47229789f
Jonathan Bautista <jb091631@gmail.com>
2025-03-29 12:15:56 -0500

    enrutador y controladores

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 /* importamos al framework express */
-import express from "express";
-
+import express from "express" ;
+import categoriasRoutes from "./routes/categorias.routes.js"
 /* asignamos a app toda la funcionalidad para mi server web */
 const app= express();
 
 /* set6ear un puesrto a mi web server */
 app.set("port",5000)
+
+/* routes */
+app.use("/api/categorias",categoriasRoutes)
 
 /* hacemos disponible a mi server app para toda la app */
 export default app;
