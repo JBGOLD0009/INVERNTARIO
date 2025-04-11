84d8c557eb08c0c08c17542829d3434d0b797b0b
Jonathan Bautista <jb091631@gmail.com>
2025-04-11 14:39:31 -0500

    Connecting frontend-backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 /* importamos al framework express */
 import express from "express" ;
+import cors from "cors"
 import categoriasRoutes from "./routes/categorias.routes.js"
 /* asignamos a app toda la funcionalidad para mi server web */
 const app= express();
@@ -7,8 +8,12 @@ const app= express();
 /* set6ear un puesrto a mi web server */
 app.set("port",5000)
 
+app.set(cors)
 /* routes */
 app.use("/api/categorias",categoriasRoutes)
-
+// Ruta raíz que entrega el index.html
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("frontend/index.html"));
+  });
 /* hacemos disponible a mi server app para toda la app */
 export default app;
