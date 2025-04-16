8a3121762cdc77c73516c9ecdb183902fb844105
Jonathan Bautista <jb091631@gmail.com>
2025-04-16 14:25:44 -0500

    Una sola fila By Id - Método HTTP GET

--- backend/src/routes/categorias.routes.js
+++ backend/src/routes/categorias.routes.js
@@ -8,5 +8,7 @@ const router = Router();
 router.get("/",categoriaController.getCategorias); /* read */
 router.post("/",categoriaController.PostCategorias);/* create*/
 
+/* Ruta que rcibe un parametro */
+router.get("/:id",categoriaController.getCategory);/* create*/
 /* hacemos disponible a mi sevrer app para toda la app */
 export default router;
